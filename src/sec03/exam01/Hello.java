5d71c0d86a00be32c6d26207b44c6b95fc5d807a
leeDragonHun <9leeyh5@gmail.com>
2024-08-26 15:18:04 +0900

    2024.08.26 주석 사용하기

--- src/sec03/exam01/Hello.java
+++ src/sec03/exam01/Hello.java
@@ -1,10 +1,13 @@
 package sec03.exam01;
 
+/*
+ 작성자: 이용훈
+ 작성일: 2024.08.26 
+ */
 public class Hello {
-
+	// 프로그램 실행 진입점
 	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
 		System.out.println("Hello Java");
-
-	}
-
-}
+	} // end of main
+} // end of class
